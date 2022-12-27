645d27de250d3219bc25ea7ce6cc47d90bffaa13
Usman <111375580+usman-m100@users.noreply.github.com>
2022-12-27 22:24:16 +0000

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,7 +17,7 @@ function Contact() {
                         className='my-2 p-2 bg-transparent border-2 rounded-md focus:outline-none' />
                     <textarea name="message" placeholder="Message" rows="10" type="text"
                     className="p-2 mb-4 bg-transparent border-2 rounded-md focus:outline-none"/>
-                    <button type='button' className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-orange-500 drop-shadow-md hover:stroke-white'>
+                    <button type='submit' className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-orange-500 drop-shadow-md hover:stroke-white'>
                         Work with me
                     </button>
                 </form>
